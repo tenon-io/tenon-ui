1ecb1a2ecd83ed82af92abac1891f0adf9b0b93a
AlmeroSteyn <almero.steyn@gmail.com>
2018-11-23 14:05:22 +0100

    Add code docs and proptypes. Export the disclosure.

--- src/modules/layout/disclosure/Trigger.js
+++ src/modules/layout/disclosure/Trigger.js
@@ -2,8 +2,27 @@ import React from 'react';
 import DisclosureContext from './DisclosureContext';
 import classNames from 'classnames';
 import { callAll } from '../../utils/helpers/functionHelpers';
+import PropTypes from 'prop-types';
 
-const Trigger = ({ onClick, children, className, expandedClass, ...props }) => (
+/**
+ * @component
+ * This component should be used as a child of the Disclosure component.
+ * It houses the trigger for toggling the expand / collapse of the
+ * Target(s).
+ *
+ * It always renders a button as this is the recommended element to use
+ * as disclosure trigger.
+ *
+ * The button accepts any content. Also a render function that injects
+ * the expanded flag to enable switching content based on the toggle
+ * state.
+ *
+ * The button accepts all standard button props.
+ *
+ * @prop {string} expandedClass - A class to add to the button when the related
+ * target is expanded.
+ */
+const Trigger = ({ children, onClick, className, expandedClass, ...props }) => (
     <DisclosureContext.Consumer>
         {({ expanded, onExpandToggleHandler }) => (
             <button
@@ -21,5 +40,9 @@ const Trigger = ({ onClick, children, className, expandedClass, ...props }) => (
         )}
     </DisclosureContext.Consumer>
 );
+
+Trigger.propTypes = {
+    expandedClass: PropTypes.string
+};
 
 export default Trigger;
